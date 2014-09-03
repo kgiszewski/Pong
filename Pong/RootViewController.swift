ba9603a6629c038fc6eec84a21b0d55f57346a91
Bryce Hammond <bryce.hammond@imulus.com>
2014-09-03 15:15:03 -0600

    Add in vs animation

--- Pong/RootViewController.swift
+++ Pong/RootViewController.swift
@@ -18,12 +18,14 @@ class RootViewController: UIViewController, PlayerSelectionViewControllerDelegat
     
     lazy var player1SelectionController : PlayerSelectionViewController = {
         let selectionController = UIStoryboard.mainStoryboard().instantiateViewControllerWithIdentifier("Player Selection") as PlayerSelectionViewController
+        selectionController.delegate = self
         self.addChildViewController(selectionController)
         return selectionController
     }()
     
     lazy var player2SelectionController : PlayerSelectionViewController = {
         let selectionController = UIStoryboard.mainStoryboard().instantiateViewControllerWithIdentifier("Player Selection") as PlayerSelectionViewController
+        selectionController.delegate = self
         self.addChildViewController(selectionController)
         return selectionController
     }()
@@ -55,24 +57,60 @@ class RootViewController: UIViewController, PlayerSelectionViewControllerDelegat
         
         if(self.player1 != nil && self.player2 != nil) {
             //both players have been selected so instigate the match
-            
+            self.performVsAnimation {
+                self.performFightAnimation {
+                    
+                }
+            }
             
         }
     }
     
     //MARK: Match animations
     
-    func performVsAnimation() {
+    func performVsAnimation( completion: Void -> Void) {
+        let vsLabel = UILabel(frame: CGRectMake(0, 0, 400, 400))
+        vsLabel.font = UIFont.boldSystemFontOfSize(200)
+        vsLabel.text = "VS"
+        vsLabel.textAlignment = .Center
+        vsLabel.textColor = UIColor.redColor()
+        vsLabel.backgroundColor = UIColor.clearColor()
+        vsLabel.center = self.view.center
+        
+        let vsImage = vsLabel.image
+        let vsImageView = UIImageView(image: vsImage)
+        vsImageView.frameSize = CGSizeZero;
+        vsImageView.center = self.view.center
+        self.view.addSubview(vsImageView);
+        
+        UIView.animateWithDuration(0.2, animations: {
+                vsImageView.frame = CGRectMake(self.view.center.x - vsImage.size.width / 2, self.view.center.y - vsImage.size.height / 2, vsImage.size.width, vsImage.size.height)
+            }, completion: { finished in
+                
+                UIView.animateWithDuration(0.2, delay: 1.5, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+                    
+                    vsImageView.frameSize = CGSizeMake(0, 0)
+                    vsImageView.center = self.view.center
+                    
+                    }, completion: { finished in
+                        if(finished)
+                        {
+                            vsImageView.removeFromSuperview()
+                            completion()
+                        }
+                })
+        })
+    }
+    
+    func performRound1Animation( completion: Void -> Void) {
         
     }
     
-    func performRound1Animation() {
+    func performFightAnimation( completion: Void -> Void) {
         
     }
     
-    func performFightAnimation() {
-        
-    }
+    
 
 }
 
